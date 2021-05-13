a8e129ce153b8c8e93c8db916988e1b1c4ac622b
akito1201 <akito.tennis@gmail.com>
2021-05-13 21:26:13 +0900

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('error');
+  res.render('index');
+});
+
+router.get('/json', function(req, res, next) {
+  res.json({ hello: "world "});
 });
 
 module.exports = router;
